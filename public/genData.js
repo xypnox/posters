1e7f57767b3c15f8d1bd3acbd5205f10531ac92c
xypnox <xypnox@gmail.com>
2019-08-23 23:22:19 +0530

    Add image compression and resize
    Now images can be compressed and resized automatically

--- public/genData.js
+++ public/genData.js
@@ -1,9 +1,12 @@
+const sharp = require('sharp');
+
 //requiring path and fs modules
 const path = require('path');
 const fs = require('fs');
 //joining path of directory
 const directoryPath = path.join(__dirname, 'images');
 //passsing directoryPath and callback function
+const compress_images = require('compress-images');
 
 let fileData = [];
 
@@ -13,6 +16,40 @@ fs.readdir(directoryPath, function(err, files) {
   }
   files.forEach(function(file) {
     fileData.push(file);
+
+    sharp('images/' + file)
+      .resize({
+        width: 600,
+        fit: 'contain'
+      })
+      .toFile('resized/' + file)
+      .then((err, info) => {
+        compress_images(
+          'resized/' + file,
+          'compressed/',
+          { compress_force: false, statistic: false, autoupdate: false },
+          false,
+          { jpg: { engine: 'mozjpeg', command: ['-quality', '60'] } },
+          { png: { engine: 'pngquant', command: ['--quality=20-50'] } },
+          { svg: { engine: 'svgo', command: '--multipass' } },
+          {
+            gif: {
+              engine: 'gifsicle',
+              command: ['--colors', '64', '--use-col=web']
+            }
+          },
+          function(error, completed, statistic) {
+            console.log('-------------');
+            if (error) console.log(error);
+            if (completed) console.log('Completed: ', completed);
+            console.log(statistic);
+            console.log('-------------');
+          }
+        );
+      })
+      .catch(err => {
+        console.log('error: ', err);
+      });
   });
 
   let jsonContent = JSON.stringify(fileData);
